b28af4303a291bd0a7790da19d3e62660b72b170
sam <sam7.yeh@advantech.com.tw>
2022-04-28 20:13:16 +0800

    day2

--- algorithm/slidingwindow.js
+++ algorithm/slidingwindow.js
@@ -1,14 +1,22 @@
-console.log(maxSum([2,7,3,0,6,1,-5,-12,-11],3))
-//console.log(MinSum([2,7,3,0,6,1,-5,-12,-11],3))
-
 let maxSum = function(arr,size){
-    let pointer = 0;
-    let end = arr.length - seq;
-    let max = 0;
-    let curSum = arr[pointer] + arr
-    while(pointer <= end){
-        if(max < arr[pointer])
+   if(size > arr.length){
+       return null;
+   }
+   let maxValue = 0;
+   for(let i=0; i<size; i++){
+    maxValue += arr[i];
+   }
+   let tmpValue = maxValue;
+   for(let j=size; j<arr.length; j++){
+    tmpValue = tmpValue + arr[j] - arr[j-size];
+    if(tmpValue>maxValue){
+        maxValue = tmpValue;
     }
+   }
+   console.log(maxValue);
+   return maxValue;
 }
+maxSum([2,7,3,0,6,1,-5,-12,-11],3)
+
 
 
